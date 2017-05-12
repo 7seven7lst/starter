f439ac658fc390ab1bf11450816fcb897224d13e
Shengtao Li <sevenlist0110@gmail.com>
2017-05-11 18:25:43 -0700

    [update] create-react-app front end and express backend skeleton

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    fetch(`/test`, {
+        accept: 'application/json',
+      })
+      .then(response=>response.text())
+      .then(parsedJSON=>{
+        console.log("Parsed JSON is >>>>", parsedJSON);
+      });
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
